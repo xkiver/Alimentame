2bd6d89d54040f50a0803a9de88f7dd2b7409ce6
claudiocampuzano <claudio.campuzano.b@gmail.com>
2016-10-07 13:43:06 -0300

    Ejemplo de geofence

---
+++ app/src/main/java/cl/telematica/android/alimentame/Constants.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cl.telematica.android.alimentame;
+
+/**
+ * Created by Claudio on 07-10-2016.
+ */
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.HashMap;
+
+/**
+ * Constantes usadas por la aplicacion.
+ */
+public final class Constants {
+
+    private Constants() {
+    }
+
+    public static final String PACKAGE_NAME = "com.google.android.gms.location.Geofence";
+
+    public static final String SHARED_PREFERENCES_NAME = PACKAGE_NAME + ".SHARED_PREFERENCES_NAME";
+
+    public static final String GEOFENCES_ADDED_KEY = PACKAGE_NAME + ".GEOFENCES_ADDED_KEY";
+
+    /**
+     * Used to set an expiration time for a geofence. After this amount of time Location Services
+     * stops tracking the geofence.
+     */
+    public static final long GEOFENCE_EXPIRATION_IN_HOURS = 12;
+
+    /**
+     * Geofence expira luego de 12 horas.
+     */
+    public static final long GEOFENCE_EXPIRATION_IN_MILLISECONDS =
+            GEOFENCE_EXPIRATION_IN_HOURS * 60 * 60 * 1000;
+    public static final float GEOFENCE_RADIUS_IN_METERS = 20;
+
+    public static final HashMap<String, LatLng> BAY_AREA_LANDMARKS = new HashMap<String, LatLng>();
+    static {
+        BAY_AREA_LANDMARKS.put("Esquina LabQui", new LatLng(-33.035341, -71.594965));
+        BAY_AREA_LANDMARKS.put("Esquina labTel", new LatLng(-33.035558, -71.595667));
+        BAY_AREA_LANDMARKS.put("Mi Casa", new LatLng(-33.049191, -71.400149));
+    }
+}
